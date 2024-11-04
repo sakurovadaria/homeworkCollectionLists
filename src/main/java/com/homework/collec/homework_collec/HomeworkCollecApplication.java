7b4865facc8fd9965f6dace13743842512ad6f4e
homework <Sakurovadaria@gmail.com>
2024-11-05 02:32:26 +0500

    First commit

---
+++ src/main/java/com/homework/collec/homework_collec/HomeworkCollecApplication.java
@@ -0,0 +1,13 @@
+package com.homework.collec.homework_collec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCollecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCollecApplication.class, args);
+	}
+
+}
